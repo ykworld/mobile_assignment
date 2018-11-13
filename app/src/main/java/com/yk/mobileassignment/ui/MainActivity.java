38326c23b9198dce79ba8654a3c74ea3d7035732
yonseungkim <yonseung.kim@gmail.com>
2018-11-13 01:50:33 -0500

    change package

---
+++ app/src/main/java/com/yk/mobileassignment/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.yk.mobileassignment.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yk.mobileassignment.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
